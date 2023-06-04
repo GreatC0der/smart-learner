a0279e9f7bcb89a37fae71d7f9eb3b0c12f4cb15
GreatC0der <satisfactio@tutanota.com>
2023-06-04 18:49:05 +0100

    Settings implemented and folder for decks setting added.

--- smart-learner-gui/src/main.rs
+++ smart-learner-gui/src/main.rs
@@ -1,10 +1,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use eframe::egui::{self, Id};
+use eframe::{egui::{self, Id}, epaint::Vec2};
+use egui_file::FileDialog;
 use smart_learner_helper::app::App;
 fn main() {
     env_logger::init();
     let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(640.0, 480.0)),
         ..Default::default()
     };
     eframe::run_native(
@@ -20,11 +22,13 @@ struct GuiApp {
     state: GuiState,
     revising_deck: usize,
     new_deck_name: String,
+    choose_folder_dialog: Option<FileDialog>,
 }
 
 enum GuiState {
     MainPage,
     Revising,
+    Settings,
 }
 
 impl Default for GuiApp {
@@ -34,6 +38,7 @@ impl Default for GuiApp {
             state: GuiState::MainPage,
             revising_deck: 0,
             new_deck_name: "".to_string(),
+            choose_folder_dialog: None,
         }
     }
 }
@@ -45,6 +50,9 @@ impl eframe::App for GuiApp {
             ui.horizontal(|ui| {
                 if ui.button("Home").clicked() {
                     self.state = GuiState::MainPage;
+                };
+                if ui.button("Settings").clicked() {
+                    self.state = GuiState::Settings;
                 };
             });
         });
@@ -77,6 +85,24 @@ impl eframe::App for GuiApp {
             GuiState::Revising => {
                 egui::CentralPanel::default().show(ctx, |ui| {
                     ui.label("Revise in your book!");
+                });
+            }
+            GuiState::Settings => {
+                egui::CentralPanel::default().show(ctx, |ui| {
+                    if ui.button("Change folder with decks").clicked() {
+                        let mut dialog = FileDialog::select_folder(None).default_size(Vec2::new(480.0, 300.0));
+                        dialog.open();
+                        self.choose_folder_dialog = Some(dialog);
+                    }
+
+                    if let Some(dialog) = &mut self.choose_folder_dialog {
+                        if dialog.show(ctx).selected() {
+                            if let Some(file) = dialog.path() {
+                                self.app.config.folder_path =
+                                    file.as_path().to_str().unwrap().to_string();
+                            }
+                        }
+                    }
                 });
             }
         }
