f7c7b6241e93c307f2f42fcca3a4b91c666e8eec
GreatC0der <132267089+GreatC0der@users.noreply.github.com>
2023-06-12 18:41:56 +0100

    Date is crossplatform now!

--- smart-learner-core/src/date.rs
+++ smart-learner-core/src/date.rs
@@ -1,4 +1,4 @@
-use std::{io::Read, process::Command};
+use chrono::{self, Datelike};
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
@@ -29,32 +29,14 @@ impl PartialOrd for Date {
 }
 
 impl Date {
-    #[cfg(target_family = "unix")]
     pub fn current() -> Self {
-        let mut string = String::new();
-        Command::new("/bin/bash")
-            .arg("-c")
-            .arg("date +%F")
-            .output()
-            .unwrap()
-            .stdout
-            .as_slice()
-            .read_to_string(&mut string)
-            .unwrap();
-
-        let bytes = string.as_bytes();
-
-        let year = &bytes[0..4];
-        let month = &bytes[5..7];
-        let day = &bytes[8..10];
+        
+        let date = chrono::offset::Local::now();
 
         Date {
-            day: ((day[0] - 48) * 10) + (day[1] - 48),
-            month: ((month[0] - 48) * 10) + (month[1] - 48),
-            year: ((year[0] as u16 - 48) * 1000)
-                + ((year[1] as u16 - 48) * 100)
-                + ((year[2] as u16 - 48) * 10)
-                + (year[3] as u16 - 48),
+            day: date.day() as u8,
+            month: date.month() as u8,
+            year: date.year() as u16,
         }
     }
 
